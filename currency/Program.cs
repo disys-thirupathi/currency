5da659fd41a783772cc5dd3411418e59743f7939
thirupathi <thirupathi.narayanan@disys.com>
2022-02-01 17:22:44 +0530

    Add project files.

---
+++ currency/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace currency
+{
+    abstract class inr_usd
+    {
+        public abstract void inrtousd();
+    }
+    abstract class inr_yen
+    {
+        public abstract void inrtoyen();
+    }
+    abstract class inr_euro
+    {
+        public abstract void inrtoeuro();
+    }
+    abstract class usd_inr
+    {
+        public abstract void usdtoinr();
+    }
+    abstract class yen_inr
+    {
+        public abstract void yentoinr();
+    }
+    abstract class euro_inr
+    {
+        public abstract void eurotoinr();
+    }
+    class inrtousdconv : inr_usd
+    {
+        public double inr = Convert.ToDouble(Console.ReadLine());
+        public double result = 0;
+        public inrtousdconv()
+        {
+            result = inr * 0.013;
+            
+        }
+        public override void inrtousd()
+        {
+            Console.WriteLine("INR in USD: " + result);
+        }
+    }
+    class inrtoyenconv : inr_yen
+    {
+        public double inr = Convert.ToDouble(Console.ReadLine());
+        public double result = 0;
+        public inrtoyenconv()
+        {
+            result = inr * 1.54;
+
+        }
+        public override void inrtoyen()
+        {
+            Console.WriteLine("INR in YEN: " + result);
+        }
+    }
+    class inrtoeuroconv : inr_euro
+    {
+        public double inr = Convert.ToDouble(Console.ReadLine());
+        public double result = 0;
+        public inrtoeuroconv()
+        {
+            result = inr * 0.012;
+
+        }
+        public override void inrtoeuro()
+        {
+            Console.WriteLine("INR in Euro: " + result);
+        }
+    }
+    class usdtoinrconv : usd_inr
+    {
+        public double usd = Convert.ToDouble(Console.ReadLine());
+        public double result = 0;
+        public usdtoinrconv()
+        {
+            result = usd * 74.29;
+
+        }
+        public override void usdtoinr()
+        {
+            Console.WriteLine("USD in INR: " + result);
+        }
+    }
+    class yentoinrconv : yen_inr
+    {
+        public double yen = Convert.ToDouble(Console.ReadLine());
+        public double result = 0;
+        public yentoinrconv()
+        {
+            result = yen * 0.65;
+
+        }
+        public override void yentoinr()
+        {
+            Console.WriteLine("YEN in INR: " + result);
+        }
+    }
+    class eurotoinrconv : euro_inr
+    {
+        public double euro = Convert.ToDouble(Console.ReadLine());
+        public double result = 0;
+        public eurotoinrconv()
+        {
+            result = euro * 74.29;
+
+        }
+        public override void eurotoinr()
+        {
+            Console.WriteLine("Euro in INR: " + result);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. INR to USD\n 2. INR to YEN\n 3. INR to Euro\n 4.USD to INR\n 5. YEN to INR\n 6. Euro to INR");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if (choice == 1)
+            {
+                inr_usd i = new inrtousdconv();
+                i.inrtousd();
+            }
+            else if (choice == 2)
+            {
+                inr_yen y = new inrtoyenconv();
+                y.inrtoyen();
+            }
+            else if (choice == 3)
+            {
+                inr_euro y = new inrtoeuroconv();
+                y.inrtoeuro();
+            }
+            else if (choice == 4)
+            {
+                usd_inr y = new usdtoinrconv();
+                y.usdtoinr();
+            }
+            else if (choice == 5)
+            {
+                yen_inr y = new yentoinrconv();
+                y.yentoinr();
+            }
+            else if (choice == 6)
+            {
+                euro_inr y = new eurotoinrconv();
+                y.eurotoinr();
+            }
+        }
+    }
+}
---
+++ currency/timeconv.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace currency
+{
+    abstract class hr_min
+    {
+        public abstract void hrtomin();
+    }
+    abstract class min_hr
+    {
+        public abstract void mintohr();
+    }
+    abstract class hr_sec
+    {
+        public abstract void hrtosec();
+    }
+    abstract class sec_hr
+    {
+        public abstract void sectohr();
+    }
+    class hrtominconv : hr_min
+    {
+        public double hr = Convert.ToDouble(Console.ReadLine());
+        public double result = 0;
+        public hrtominconv()
+        {
+            result = hr * 60;
+
+        }
+        public override void hrtomin()
+        {
+            Console.WriteLine("Hour in Minutes: " + result);
+        }
+    }
+    class mintohrconv : min_hr
+    {
+        public double min = Convert.ToDouble(Console.ReadLine());
+        public double result = 0;
+        public mintohrconv()
+        {
+            result = min / 60;
+
+        }
+        public override void mintohr()
+        {
+            Console.WriteLine("Minutes in Hours: " + result);
+        }
+    }
+    class hrtosecconv : hr_sec
+    {
+        public double hr = Convert.ToDouble(Console.ReadLine());
+        public double result = 0;
+        public hrtosecconv()
+        {
+            result = hr * 3600;
+
+        }
+        public override void hrtosec()
+        {
+            Console.WriteLine("Hour in seconds: " + result);
+        }
+    }
+    class sectohrconv : sec_hr
+    {
+        public double sec = Convert.ToDouble(Console.ReadLine());
+        public double result = 0;
+        public sectohrconv()
+        {
+            result = sec / 3600;
+
+        }
+        public override void sectohr()
+        {
+            Console.WriteLine("Seconds in hour: " + result);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Hour to Minutes\n 2. Minutes to Hour\n 3. Hour to seconds\n 4.Seconds to Hour\n");
+
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if (choice == 1)
+            {
+                hr_min i = new hrtominconv();
+                i.hrtomin();
+            }
+            else if (choice == 2)
+            {
+                min_hr y = new mintohrconv();
+                y.mintohr();
+            }
+            else if (choice == 3)
+            {
+                hr_sec y = new hrtosecconv();
+                y.hrtosec();
+            }
+            else if (choice == 4)
+            {
+                sec_hr y = new sectohrconv();
+                y.sectohr();
+            }
+        }
+    }
+}
